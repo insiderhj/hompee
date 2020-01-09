f9c365ec348dcb7adda549a76b720bb21cd54caf
insiderhj <insiderhj@gmail.com>
2020-01-09 17:25:49 +0900

    remove unnecessary initialization

--- src/main/java/com/practice/hompee/service/MailConfirmService.java
+++ src/main/java/com/practice/hompee/service/MailConfirmService.java
@@ -18,7 +18,7 @@ public class MailConfirmService {
     public String getConfirmKey() {
         Random rand = new Random();
         StringBuffer sb = new StringBuffer();
-        int num = 0;
+        int num;
         do {
             num = rand.nextInt(43) + 48;
             if (num >= 48 && num < 57 || num >= 65 && num <= 90) sb.append((char) num);
