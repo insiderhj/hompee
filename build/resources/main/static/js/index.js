11cac27f101f116d0285e4da448dd4abbe4d9a68
insiderhj <insiderhj@gmail.com>
2020-01-13 21:15:27 +0900

    add gitignore

--- build/resources/main/static/js/index.js
+++
@@ -1,8 +0,0 @@
-$('#closeAccount').on('click', function() {
-    var confirmResult = confirm("정말 탈퇴하시겠습니까?");
-    if (!confirmResult) {
-        event.preventDefault();
-        return
-    }
-    alert('회원탈퇴가 완료되었습니다.');
-});
